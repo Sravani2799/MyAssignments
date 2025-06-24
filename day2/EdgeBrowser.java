20cc0928919c96e5bd4fa0e34546ffb89de52938
Sravani2799 <sravanideviparamasivam@gmail.com>
2025-06-24 12:16:04 +0530

    Add files via upload

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		
+		Browser obj= new Browser();
+		System.out.println(obj.launchbrowser("Edge"));
+		obj.loadUrl();
+	}
+}
